5e7e2556f1db8cd3abea4c41d4b58eb1959c355b
Satin91 <cool19912015@gmail.com>
2025-08-28 17:29:19 +0300

    Initial Commit

---
+++ Lynz/LynzApp.swift
@@ -0,0 +1,17 @@
+//
+//  LynzApp.swift
+//  Lynz
+//
+//  Created by Артур Кулик on 28.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct LynzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
