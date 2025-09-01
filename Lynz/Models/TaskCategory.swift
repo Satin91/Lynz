a84f8a856c13aa27cc77a99f864013654682d054
Satin91 <cool19912015@gmail.com>
2025-09-01 19:34:43 +0300

    Decompose Plan Model

---
+++ Lynz/Models/TaskCategory.swift
@@ -0,0 +1,13 @@
+//
+//  TaskCategory.swift
+//  Lynz
+//
+//  Created by Артур Кулик on 01.09.2025.
+//
+
+import Foundation
+
+struct TaskCategory: Codable, Hashable {
+    var name: String
+    var isActive: Bool
+}
